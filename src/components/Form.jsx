30a999b355a9c41cea4650d3932748683c2f6f58
juanrojasaguilar <yo217@hotmail.es>
2024-05-28 12:20:16 -0500

    :arrow_up: react form added to dependencies

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,39 @@
 import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
 export default function ReactForm() {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({ criteriaMode: "all" });
   const onSubmit = data => console.log(data);
-
-  console.log(watch("example"));
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input defaultValue="test" {...register("example")} />
-      
-      <input {...register("exampleRequired", { required: true })} />
-      {errors.exampleRequired && <span>This field is required</span>}
-      
+      <fieldset>
+        <label>Precio: </label>
+        <input placeholder="precio" {...register("example")} />
+      </fieldset>
+      <fieldset>
+        <label>Proveedor: </label>
+        <input placeholder="precio" />
+      </fieldset>
+      <fieldset>
+        <label>Cantidades: </label>
+        <input
+          placeholder="Ingrese la cantidad"
+          {...register("multipleErrorInput", {
+            required: true,
+            pattern: {
+              value: /d+/,
+              message: "Por favor, escribe un numero"
+            }
+          })} />
+      </fieldset>
+      <ErrorMessage
+        errors={errors}
+        name="singleErrorInput"
+        render={({ message }) => <p>{message}</p>}
+        
+      />
+
       <input type="submit" />
     </form>
   );

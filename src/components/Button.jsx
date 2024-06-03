f390754ba94334cffed0aa4f79949ded3509cc3a
Jhon-Roa <roajhon98@gmail.com>
2024-06-02 21:54:08 -0500

    :art: se añadio responsividad

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ export const Button = ({ page }) => {
   return (
     <a className="flex justify-center hover:translate-x-10 w-3/5 transition-all" href={page.url}>
       <button
-        className="text-white font-semibold rounded-md bg-cyan w-full h-12 landscape:sm:h-15 landscape:lg:h-20  portrait:sm:h-20 truncate text-[25px] md:text-[35px]"
+        className="text-white font-semibold rounded-md bg-cyan w-full h-12 landscape:sm:h-15 landscape:lg:h-20 landscape:2xl:h-32  portrait:sm:h-20 truncate text-[25px] landscape:2xl:text-[50px] md:text-[35px]"
       >
         {page.title}
       </button>
